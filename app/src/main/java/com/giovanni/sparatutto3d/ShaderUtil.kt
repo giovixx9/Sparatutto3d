175e70a6127eaec3bb3feda65da424969a013d37
giovixx9 <giovannimaiaru36@gmail.com>
2026-08-07 10:43:23 +0200

    Create ShaderUtil.kt

---
+++ app/src/main/java/com/giovanni/sparatutto3d/ShaderUtil.kt
@@ -0,0 +1,22 @@
+package com.giovanni.sparatutto3d
+
+import android.opengl.GLES20
+
+object ShaderUtil {
+    fun compile(type: Int, src: String): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, src)
+        GLES20.glCompileShader(shader)
+        return shader
+    }
+
+    fun buildProgram(vertexSrc: String, fragmentSrc: String): Int {
+        val vs = compile(GLES20.GL_VERTEX_SHADER, vertexSrc)
+        val fs = compile(GLES20.GL_FRAGMENT_SHADER, fragmentSrc)
+        val program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vs)
+        GLES20.glAttachShader(program, fs)
+        GLES20.glLinkProgram(program)
+        return program
+    }
+}
